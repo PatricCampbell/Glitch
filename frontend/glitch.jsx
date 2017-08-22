18923677475cb34a9e78fa15179bbac0f3e07f94
patric <patric@patriccampbell.com>
2017-08-22 09:24:38 -0400

    Verify entry file to React works

---
+++ frontend/glitch.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.querySelector('#root');
+  ReactDOM.render(<h1>Welcome to Glitch</h1>, root);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
   entry: './frontend/glitch.jsx',
   output: {
-    filename: '/app/assets/javascripts',
+    filename: './app/assets/javascripts/bundle.js',
   },
   module: {
     loaders: [
