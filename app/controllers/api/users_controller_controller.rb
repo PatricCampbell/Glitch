7b4cce455e7ee21308222900f029db47fc8aa335
patric <patric@patriccampbell.com>
2017-08-22 09:50:41 -0400

    Add user controller for backend auth

--- app/controllers/api/users_controller_controller.rb
+++ app/controllers/api/users_controller_controller.rb
@@ -1,2 +1,17 @@
 class Api::UsersControllerController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors, status: 401
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:user).require(:username, :password)
+  end
 end
