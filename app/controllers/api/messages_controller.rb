836e83c1055bb72c87584e45128236d81526f7c2
patric <patric@patriccampbell.com>
2017-08-23 12:00:40 -0400

    Add message controller and index api view

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,3 +1,16 @@
 class Api::MessagesController < ApplicationController
+  def index
+    @messages = Message.all.includes(:author)
+  end
 
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:author_id, :body)
+  end
 end
