9535e4cec70fde58f12adc9c12672dbaba313684
patric <patric@patriccampbell.com>
2017-08-23 11:31:36 -0400

    Add message api routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :users, only: [:create]
     resource :sessions, only: [:create, :destroy]
+    resources :messages, only: [:index, :create, :destroy]
   end
 end
