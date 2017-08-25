4e43b7552f929ddcd615a7e6ff9c9759af8f56cc
patric <patric@patriccampbell.com>
2017-08-25 09:55:04 -0400

    Add placeholder image to messages and style them to fit together with the message

--- frontend/components/channels/messageListItem.jsx
+++ frontend/components/channels/messageListItem.jsx
@@ -4,13 +4,18 @@ const MessageListItem = props => {
   const { body } = props.message;
 
   return (
-    <li className='message'>
-      <span className='author-username bold'>
-        {props.message.author.username}
-      </span>
-      <span className='message-body'>
-        {body}
-      </span>
+    <li className='message-item'>
+      <div>
+        <img src='http://placebear.com/36/36' />
+      </div>
+      <div className='message'>
+        <span className='author-username bold'>
+         {props.message.author.username}
+        </span>
+        <span className='message-body'>
+          {body}
+        </span>
+      </div>  
     </li>
   );
 };
