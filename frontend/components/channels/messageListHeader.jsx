4babdd2d84207384a2ddc2357ad5d4ee332e1e7f
patric <patric@patriccampbell.com>
2017-08-29 12:28:15 -0400

    Add ability to open edit form for a channel

--- frontend/components/channels/messageListHeader.jsx
+++ frontend/components/channels/messageListHeader.jsx
@@ -1,41 +1,79 @@
 import React from 'react';
+import ChannelFormContainer from '../modals/channelFormContainer';
 
-const MessageListHeader = props => {
-  const channel = props.channels[props.match.params.channel_id];
+class MessageListHeader extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const handleDelete = e => {
+    this.state = {
+      channel: null,
+      formState: 'hidden',
+      formType: 'edit',
+    }
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleCloseForm = this.handleCloseForm.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      channel: nextProps.channels[nextProps.match.params.channel_id]
+    })
+  }
+
+  handleDelete(e) {
     props.deleteChannel(channel)
       .then(() => {
         const firstChannelId = Object.keys(props.channels)[0];
         props.history.push(`/channels/${firstChannelId}`)
       });
-  };
-  
-  if (channel) {
-    return (
-      <div className='message-list-header'>
-        <p className='bold'>
-          # {channel.name}
-        </p>
-        <p>
-          {channel.description}
-        </p>
-        {channel.creator_id === props.currentUser.id &&
-          <div>  
-            <button>Edit
-            </button>
-            <button onClick={handleDelete}>Delete
-            </button>
-          </div>
-        }
-      </div>
-    );
-  } else {
-    return (
-      <div className='message-list-header'>
-      </div>
-    );
   }
+
+  handleEdit(e) {
+    this.setState({
+      formState: 'shown',
+    });
+  }
+
+  handleCloseForm(e) {
+    this.setState({
+      formState: 'hidden',
+    });
+  }
+
+  render() {
+    if (this.state.channel) {
+      return (
+        <div className='message-list-header'>
+          <p className='bold'>
+            # {this.state.channel.name}
+          </p>
+          <p>
+            {this.state.channel.description}
+          </p>
+          {this.state.channel.creator_id === this.props.currentUser.id &&
+            <div>  
+              <button onClick={this.handleEdit}>Edit
+              </button>
+              <button onClick={this.handleDelete}>Delete
+              </button>
+            </div>
+          }
+          {this.state.formState === 'shown' ? <ChannelFormContainer
+            formType={this.state.formType}
+            handleCloseForm={this.handleCloseForm}
+            channel={this.state.channel}
+          /> : null}
+        </div>
+      );
+    } else {
+      return (
+        <div className='message-list-header'>
+        </div>
+      );
+    }
+  } 
 };
 
 export default MessageListHeader;
