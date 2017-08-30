a94de5c461b52639a70304bc00ffb4b94ed3ca55
patric <patric@patriccampbell.com>
2017-08-30 16:11:53 -0400

    Add index route and action to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resource :users, only: [:create]
+    resources :users, only: [:index, :create]
     resource :sessions, only: [:create, :destroy]
     resources :channels, only: [:index, :create, :update, :destroy] do 
       resources :messages, only: [:index, :create, :destroy]
