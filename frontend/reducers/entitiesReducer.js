a04a07e107b835d0b8e4c0d95ea1ce1db936b4df
patric <patric@patriccampbell.com>
2017-08-30 16:20:56 -0400

    Add user actions, apiutil, and reducer

--- frontend/reducers/entitiesReducer.js
+++ frontend/reducers/entitiesReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import MessagesReducer from './messagesReducer';
 import ChannelsReducer from './channelsReducer';
 import DirectMessagesReducer from './directMessagesReducer';
+import UsersReducer from './usersReducer';
 
 const entitiesReducer = combineReducers({
   messages: MessagesReducer,
   channels: ChannelsReducer,
   directMessages: DirectMessagesReducer,
+  users: UsersReducer,
 });
 
 export default entitiesReducer;
--- frontend/reducers/rootReducer.js
+++ frontend/reducers/rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './sessionReducer';
 import EntitiesReducer from './entitiesReducer';
-import UiReducer from './uiReducer';
 
 const rootReducer = combineReducers({
   session: SessionReducer,
   entities: EntitiesReducer,
-  ui: UiReducer,
 });
 
 export default rootReducer;
